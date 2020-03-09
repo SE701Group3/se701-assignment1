fb194c32b55dbb83ce526219ac6fdda8d85487fa
Andrew <andrew.h318@hotmail.co.nz>
2020-03-10 12:02:59 +1300

    add mongodb boilerplate- add basic create/get endpoints

---
+++ server/routes/posts.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/post");
+
+// Get all posts
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Create one post
+router.post("/", async (req, res) => {
+  const post = new Post({
+    name: req.body.name
+  });
+
+  try {
+    const newPost = await post.save();
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Update one post
+router.patch("/:id", (req, res) => {});
+
+// Deleting one post
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;
