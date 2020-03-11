0e22d7d76799dfa9f306783265984c7c90c92159
WaiHaiDereMAC <tyger.kong@hotmail.com>
2020-03-11 17:38:02 +1300

    initial route setup and lint packages added

--- se701-frontend/src/App.js
+++ se701-frontend/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+
+
+import Frontpage from "./pages/Frontpage";
+import CreatePost from "./pages/CreatePost"
 import './App.css';
+
+const paths = {
+  FRONTPAGE: '/',
+  CREATE_POST: '/new'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Switch>
+      <Route exact path={paths.FRONTPAGE} component={Frontpage} />
+      <Route exact path={paths.CREATE_POST} component={CreatePost} />
+    </Switch>
+    </Router>
   );
 }
 
