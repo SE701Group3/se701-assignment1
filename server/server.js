eb7431d068e9008fe42576ff353c23cfdce08473
Andrew Hu <38745667+andrewh318@users.noreply.github.com>
2020-03-12 15:49:15 +1300

    Issue/#8 set up mongodb (#10)
    * add npm to project
    
    * add express boiler plate- hello world!
    
    * add mongodb boilerplate- add basic create/get endpoints
    
    * fix merge conflicts
    
    * remove old index.js file

---
+++ server/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 5001;
+
+// Set up database connection
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("connected to database"));
+
+app.use(express.json());
+
+const postsRouter = require("./routes/posts");
+app.use("/posts", postsRouter);
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
