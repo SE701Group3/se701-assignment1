e93874bccf69e4786f1746db0a4f6e0403826083
Tyger Kong <40849970+WaiHaiDere@users.noreply.github.com>
2020-03-12 15:24:56 +1300

    Setup/routing (#20)
    * created base react app
    
    * gitignore for root added
    
    * initial route setup and lint packages added
    
    * Routes set up and also linting set up
    
    * renamed to frontend
    
    * Changed trailing comma rule and added material ui
    
    * fixed routing

---
+++ frontend/src/pages/Frontpage/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>This is the front page</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
