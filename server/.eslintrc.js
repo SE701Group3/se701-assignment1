e29932cb9026faa3c35193cf4b4ab0e69dc34ba1
Ernest Wong <sudo@ernestwong.nz>
2020-03-13 23:32:33 +1300

    Setup eslint for backend
    Adds npm run lint and npm run lint-fix commands.
    Using airbnb-base instead of airbnb because we don't need react linting.

---
+++ server/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "extends": [
+    "airbnb-base",
+    "plugin:prettier/recommended",
+  ],
+  "env": {
+    "jest": true,
+    "node": true
+  },
+  "rules": {
+    "max-len": [
+      "warn",
+      {
+        "code": 100,
+        "tabWidth": 2,
+        "comments": 100,
+        "ignoreComments": false,
+        "ignoreTrailingComments": true,
+        "ignoreUrls": true,
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true,
+        "ignoreRegExpLiterals": true
+      }
+    ]
+  }
+}
