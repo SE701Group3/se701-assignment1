48727cc03af43de9189a4811b023c057d4482b36
WaiHaiDereMAC <tyger.kong@hotmail.com>
2020-03-13 16:18:26 +1300

    Added handleSearch to index.jsx

--- frontend/src/pages/Frontpage/index.jsx
+++ frontend/src/pages/Frontpage/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from "../../common/Header/Header";
-import { Container } from "@material-ui/core";
+import Header from '../../common/Header/Header';
+import { Container } from '@material-ui/core';
 
-const Index = ({ postsToDisplay }) => {
+const Index = ({ postsToDisplay, handleSearch }) => {
   return (
     <header className="App-header">
-      <Header postsToDisplay={postsToDisplay} />
+      <Header postsToDisplay={postsToDisplay} handleSearch={handleSearch} />
       <Container maxWidth="sm">
         <p>This is the front page</p>
         <a
