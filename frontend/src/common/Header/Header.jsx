586489e8b119d4a62270537442220798ceab6e90
mpop344 <42855125+mpop344@users.noreply.github.com>
2020-03-13 13:22:18 +1300

    Just added a TextField to the header
    Added a TextField to the header with a little search icon.

--- frontend/src/common/Header/Header.jsx
+++ frontend/src/common/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from "react";
-
 import styles from "./Header.module.css";
+import { TextField } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
   return (
     <div className={styles["form-header"]}>
       <span className={styles.logo}>Not-Reddit</span>
+      <form>
+        <TextField
+          id="search-bar"
+          size="small"
+          variant="outlined"
+          placeholder="Search"
+          InputProps={{
+            startAdornment: <SearchIcon />
+          }}
+        />
+      </form>
       <div className={styles["right-items"]}>
         <span>Profile</span>
       </div>
