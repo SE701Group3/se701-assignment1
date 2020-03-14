b7424be698c5e4b68e6e70c79cb1b57f61947ebc
Andrew Hu <38745667+andrewh318@users.noreply.github.com>
2020-03-14 19:16:29 +1300

    Issue/#31 Set up Jest unit tests (#34)
    * add npm to project
    
    * add express boiler plate- hello world!
    
    * add mongodb boilerplate- basic create/get endpoints
    
    * fix malformed package lock json file
    
    * add jest config + basic unit tests using in-memory mongodb
    
    * close app connection after tests and clear db between tests
    
    * add example code to test failure case
    
    * add example model tests &  separate from integration tests
    
    * abstracted initialization of db (and server) from general express configuration
    
    * add test env variable to model test
    
    * moved db related files together
    
    * simplify promise structure for db/index.js
    
    * move db drop logic into db/index.js

---
+++ server/test/integration/post.spec.js
@@ -0,0 +1,55 @@
+// Environment variable used to set the in-memory database when the server is instantiated
+process.env.NODE_ENV = "test";
+
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../../app");
+const db = require("../../src/db");
+
+describe("Posts API", () => {
+  beforeAll(async done => {
+    db.connect()
+      .then(() => done())
+      .catch(err => done(err));
+  });
+
+  beforeEach(async done => {
+    // clear database after each test to remove any dependencies between tests
+    db.drop()
+      .then(() => done())
+      .catch(err => done(err));
+  });
+
+  afterAll(async done => {
+    db.close()
+      .then(() => done())
+      .catch(err => done(err));
+  });
+
+  it("tests the create post endpoint and returns as success message", async done => {
+    const postData = {
+      name: "Test post"
+    };
+
+    const response = await supertest(app)
+      .post("/posts")
+      .send(postData);
+
+    expect(response.status).toBe(201);
+
+    const createdPost = response.body;
+    expect(createdPost.name).toBe(postData.name);
+    done();
+  });
+
+  it("tests the create post endpoint with no name and returns as error message", async done => {
+    const postData = {};
+
+    const response = await supertest(app)
+      .post("/posts")
+      .send(postData);
+
+    expect(response.status).toBe(400);
+    done();
+  });
+});
---
+++ server/test/model/post.spec.js
@@ -0,0 +1,52 @@
+process.env.NODE_ENV = "test";
+
+const mongoose = require("mongoose");
+const PostModel = require("../../src/db/models/post");
+const db = require("../../src/db");
+
+describe("Post Model", () => {
+  beforeAll(async done => {
+    db.connect()
+      .then(() => done())
+      .catch(err => done(err));
+  });
+
+  beforeEach(async done => {
+    // clear database after each test to remove any dependencies between tests
+    db.drop()
+      .then(() => done())
+      .catch(err => done(err));
+  });
+
+  afterAll(async done => {
+    db.close()
+      .then(() => done())
+      .catch(err => done(err));
+  });
+
+  it("create post and save successfully", async done => {
+    const postData = {
+      name: "Test postysdfdsfdsf"
+    };
+    const validPost = new PostModel(postData);
+    const savedPost = await validPost.save();
+
+    expect(savedPost._id).toBeDefined();
+    expect(savedPost.name).toBe(postData.name);
+
+    done();
+  });
+
+  it("create post without required fields should fail", async done => {
+    const postWithoutRequiredField = new PostModel({});
+    let err;
+    try {
+      await postWithoutRequiredField.save();
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+    done();
+  });
+});
