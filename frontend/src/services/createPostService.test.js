dbf0a09bf946a6b48ffc011eb026e7d3053a9a0f
Ernest Wong <sudo@ernestwong.nz>
2020-03-14 21:04:42 +1300

    Add createPostService tests

---
+++ frontend/src/services/createPostService.test.js
@@ -0,0 +1,39 @@
+import { enableFetchMocks } from 'jest-fetch-mock';
+
+enableFetchMocks();
+
+import submitPost from './createPostService'; // eslint-disable-line import/first
+
+process.on('unhandledRejection', up => {
+  throw up;
+});
+
+describe('submitPost', () => {
+  it('sends a POST request to the right endpoint', async () => {
+    fetch.mockResponseOnce('', {
+      status: 200,
+      statusText: 'OK',
+    });
+    await submitPost('title', 'body');
+    expect(fetch.mock.calls[0][0]).toEqual('/api/post/create');
+    expect(fetch.mock.calls[0][1].method).toEqual('POST');
+  });
+
+  it('sends the right parameters to the request', async () => {
+    fetch.mockResponseOnce('', {
+      status: 200,
+      statusText: 'OK',
+    });
+    await submitPost('title', 'body');
+    expect(fetch.mock.calls[0][1].body.title).toEqual('title');
+    expect(fetch.mock.calls[0][1].body.body).toEqual('body');
+  });
+
+  it('throws an error with the right message when the request fails', async () => {
+    fetch.mockResponseOnce('{ "message": "Missing title" }', {
+      status: 400,
+      statusText: 'Bad Request',
+    });
+    await expect(submitPost('', 'body')).rejects.toThrow('Missing title');
+  });
+});
