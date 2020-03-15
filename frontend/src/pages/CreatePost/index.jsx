1e04428619faeed903a522a1e7a0bc8cb64a026d
Ernest Wong <sudo@ernestwong.nz>
2020-03-15 15:15:05 +1300

    Issue/#38 Implement CreatePostContainer and service (#40)
    * Implement CreatePostContainer and service
    
    * Speedup lint/lint-fix commands for frontend
    
    Previously, eslint would glob the entire node_modules folder even when
    it doesn't need it.
    
    * Fix rather than suppress export default linting error
    
    * Let createPostService handle fetch response
    
    * Default to null for no error message
    
    * Use explicit post arguments
    
    Ensures parameter names for the POST request are correct and
    self-documenting.
    
    * Inject fetch dependency to createPostService
    
    * Revert "Inject fetch dependency to createPostService"
    
    This reverts commit 6f9461be2ae228522bf0b6566abda53f7b56aa06.
    
    * Add createPostService tests
    
    * Replace container component with higher order component
    
    This is so that the CreatePost presentational component can have
    PropTypes enforced, and to aid testing.
    
    * Add tests for withCreatePostService HOC
    
    * Document reason for eslint suppression after mock enable
    
    * Mock and test post creation time
    
    * Fix typo classname -> className
    
    * Remove prop-types for now to simplify development
    
    * Remove unused CreatePost dummy component

--- frontend/src/pages/CreatePost/index.jsx
+++ frontend/src/pages/CreatePost/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import CreatePostModal from './CreatePostModal';
+import withCreatePostService from './withCreatePostService';
+
+const CreatePostModalServiced = withCreatePostService(CreatePostModal);
 
 export default () => {
   return (
     <div>
       <p>asdfasdf</p>
-      <CreatePostModal />
+      <CreatePostModalServiced />
     </div>
   );
 };
