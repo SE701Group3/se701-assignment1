3c744ced7d52735ee808939cb78e67db59610fc0
AXon <seismiccat@protonmail.com>
2020-03-16 13:14:06 +1300

    add to index

--- frontend/src/pages/Frontpage/index.jsx
+++ frontend/src/pages/Frontpage/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../../common/Header/Header";
+import Post from "./Post";
 import { Container } from "@material-ui/core";
 
 export default () => {
@@ -8,6 +9,21 @@ export default () => {
       {/* <img src={logo} className="App-logo" alt="logo" /> */}
       <Header />
       <Container maxWidth="sm">
+        <Post
+          title="Title One"
+          date="28347928"
+          content="Ea consequatur animi ut sapiente accusantium aut. Eveniet cum sit dolorum vero adipisci totam accusantium suscipit."
+        />
+        <Post
+          title="Another Post"
+          date="28347928"
+          content="Ea consequatur animi ut sapiente accusantium aut. Eveniet cum sit dolorum vero adipisci totam accusantium suscipit."
+        />
+        <Post
+          title="Post Num 3"
+          date="28347928"
+          content="Ea consequatur animi ut sapiente accusantium aut. Eveniet cum sit dolorum vero adipisci totam accusantium suscipit."
+        />
         <p>This is the front page</p>
         <a
           className="App-link"
