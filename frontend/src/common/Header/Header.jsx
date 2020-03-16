62cbcfb1aabb20835992bac42672a307f18537d8
WaiHaiDereMAC <tyger.kong@hotmail.com>
2020-03-16 14:07:48 +1300

    Search bar/header styling

--- frontend/src/common/Header/Header.jsx
+++ frontend/src/common/Header/Header.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { TextField } from '@material-ui/core';
+import { TextField, AppBar, Typography, Toolbar } from '@material-ui/core';
 
 import styles from './Header.module.css';
 
 const Header = ({ handleSearch }) => {
   return (
-    <div className={styles['form-header']}>
-      <span className={styles.logo}>Not-Reddit</span>
-      <TextField onChange={handleSearch} variant="outlined" />
-
-      <div className={styles['right-items']}>
-        <span>Profile</span>
-      </div>
-    </div>
+    <AppBar position="static" classes={{ root: styles.appBar }}>
+      <Toolbar>
+        <Typography variant="h6" classes={{ root: styles.title }}>
+          Threader
+        </Typography>
+        <TextField
+          classes={{ root: styles.searchBar }}
+          onChange={handleSearch}
+          variant="outlined"
+          placeholder="Search"
+        />
+      </Toolbar>
+    </AppBar>
   );
 };
 
