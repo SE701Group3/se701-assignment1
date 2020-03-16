88a85e89a92579021a4bb5469586aabf5083cdee
rodgergit <rodgerthatxd@gmail.com>
2020-03-16 17:19:13 +1300

    Premerge changes

--- server/src/db/models/comments.js
+++ server/src/db/models/comments.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const postSchema = new mongoose.Schema({
+const commentsSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
 });
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model('Post', commentsSchema);
