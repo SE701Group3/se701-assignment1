f9d545d33acaffd4f17b20fd4cacb014f4d84476
Frank Wang Ma <frankwangma@gmail.com>
2020-03-16 17:47:07 +1300

    Fixed small issue with class naming

--- frontend/src/pages/PostDetail/index.jsx
+++ frontend/src/pages/PostDetail/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PostDetail from './PostDetail';
-import CommentContainer from './CommentsContainer';
+import CommentsContainer from './CommentsContainer';
 import CreateCommentModal from './CreateCommentModal';
 
 export default () => {
