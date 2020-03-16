6167ab1d95c494a683b40f51379b89b85a6008f6
Ernest Wong <sudo@ernestwong.nz>
2020-03-16 16:08:44 +1300

    Integrate createPostService into modal and fix fetch params (#51)

--- frontend/src/pages/CreatePost/withCreatePostService.test.js
+++ frontend/src/pages/CreatePost/withCreatePostService.test.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import { render } from '@testing-library/react';
+import { render, wait } from '@testing-library/react';
 import { createPostService } from './withCreatePostService';
+import { SubmitPostError } from '../../services/createPostService';
 
 const MockCreatePost = ({ errorMessage, onSubmit }) => {
   useEffect(() => {
@@ -10,21 +11,23 @@ const MockCreatePost = ({ errorMessage, onSubmit }) => {
 };
 
 describe('withCreatePostService', () => {
-  it('renders the wrapped component', () => {
+  it('renders the wrapped component', async () => {
     const mockSubmitPost = () => {};
     const CreatePostServiced = createPostService(mockSubmitPost)(MockCreatePost);
     const { getByText } = render(<CreatePostServiced />);
     const child = getByText(/MockCreatePost/i);
     expect(child).toBeInTheDocument();
+    await wait();
   });
 
-  it('supplies an error message when there is an error in submiting a post', () => {
+  it('supplies an error message when there is an error in submiting a post', async () => {
     const mockSubmitPost = jest.fn(() => {
-      throw new Error('an error message');
+      throw new SubmitPostError('an error message');
     });
     const CreatePostServiced = createPostService(mockSubmitPost)(MockCreatePost);
     const { getByText } = render(<CreatePostServiced />);
     const child = getByText(/an error message/i);
     expect(child).toBeInTheDocument();
+    await wait();
   });
 });
--- frontend/src/services/createPostService.js
+++ frontend/src/services/createPostService.js
@@ -1,3 +1,5 @@
+export class SubmitPostError extends Error {}
+
 export default async (title, body) => {
   const parameters = {
     title,
@@ -5,10 +7,12 @@ export default async (title, body) => {
     sender_created_at: new Date().toISOString(),
   };
 
-  const response = await fetch('/api/post/create', { method: 'POST', body: parameters });
+  const requestBody = JSON.stringify(parameters);
+
+  const response = await fetch('/api/post/create', { method: 'POST', body: requestBody });
 
   if (!response.ok) {
     const { message } = await response.json();
-    throw new Error(message);
+    throw new SubmitPostError(message);
   }
 };
