50ae8854499381441cb4fc53fd0d4d580271aad5
Frank Wang Ma <frankwangma@gmail.com>
2020-03-16 17:46:05 +1300

    Added initial classes for the Post-detail page

---
+++ frontend/src/pages/PostDetail/CreateCommentModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreateCommentModal = () => {
+  return <div>Placeholder</div>;
+};
+
+export default CreateCommentModal;
---
+++ frontend/src/pages/PostDetail/PostDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostDetail = () => {
+  return <div>Placeholder</div>;
+};
+
+export default PostDetail;
