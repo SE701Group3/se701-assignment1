2b0a84d0931d1b7441557fdfcb7accbb4fbbfa66
Frank Wang Ma <frankwangma@gmail.com>
2020-03-17 21:53:58 +1300

    Added reply button to each comment

--- frontend/src/pages/PostDetail/Comment.jsx
+++ frontend/src/pages/PostDetail/Comment.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import ReplyImage from '../../common/icons/reply.png';
 
 import styles from './Comment.module.css';
 // {this.props.post.text.split("\n").map((i, key) => {
@@ -11,6 +13,9 @@ const Comment = ({ body, dateCreated }) => {
   return (
     <Card className={styles.root}>
       <div className={styles['comment-box']}>
+        <Button>
+          <img src={ReplyImage} alt="reply-img" className={styles.reply} />
+        </Button>
         <CardContent>
           <div className={styles['comment-content']}>
             <Typography variant="body1">AnonUser</Typography>
