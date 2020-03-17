936dfa9b6399d61447c142a1f003b7a086c20d6e
Clayton Lan <Clayton.dev21@gmail.com>
2020-03-18 05:57:12 +1300

    Added unit tests for upvoting

--- server/test/integration/post.spec.js
+++ server/test/integration/post.spec.js
@@ -59,4 +59,106 @@ describe('Posts API', () => {
     expect(response.status).toBe(400);
     done();
   });
+
+  it('upvote post successfully', async done => {
+    // create post
+    const postData = {
+      title: 'Test post',
+      body: 'This is the body for a test post',
+    };
+
+    const response = await supertest(app)
+      .post('/posts')
+      .send(postData);
+
+    expect(response.status).toBe(201);
+
+    // Get post
+    const response2 = await supertest(app).get('/posts');
+
+    // Upvote the post
+    const upvoteRequest = {
+      id: response2.body[0]._id,
+      upvote_type: 'clap',
+      upvote: true,
+    };
+
+    const response3 = await supertest(app)
+      .put(`/posts/${response2.body[0]._id}/upvote`)
+      .send(upvoteRequest);
+    expect(response3.status).toBe(200);
+
+    // Check if post was upvoted
+    const response4 = await supertest(app).get('/posts');
+    expect(response4.body[0].upvotes_clap).toBe(1);
+    done();
+  });
+
+  it('upvote post with invalid type fails', async done => {
+    // create post
+    const postData = {
+      title: 'Test post',
+      body: 'This is the body for a test post',
+    };
+
+    const response = await supertest(app)
+      .post('/posts')
+      .send(postData);
+
+    expect(response.status).toBe(201);
+
+    // Get post
+    const response2 = await supertest(app).get('/posts');
+
+    // Upvote the post
+    const upvoteRequest = {
+      id: response2.body[0]._id,
+      upvote_type: 'claps',
+      upvote: true,
+    };
+
+    const response3 = await supertest(app)
+      .put(`/posts/${response2.body[0]._id}/upvote`)
+      .send(upvoteRequest);
+    expect(response3.status).toBe(400);
+
+    // Check if post was upvoted
+    const response4 = await supertest(app).get('/posts');
+    expect(response4.body[0].upvotes_clap).toBe(0);
+    done();
+  });
+
+  it('downvote post successfully', async done => {
+    // create post
+    const postData = {
+      title: 'Test post',
+      body: 'This is the body for a test post',
+    };
+
+    const response = await supertest(app)
+      .post('/posts')
+      .send(postData);
+
+    expect(response.status).toBe(201);
+
+    // Get post
+    const response2 = await supertest(app).get('/posts');
+
+    // Downvote the post
+    const upvoteRequest = {
+      id: response2.body[0]._id,
+      upvote_type: 'clap',
+      upvote: false,
+    };
+
+    const response3 = await supertest(app)
+      .put(`/posts/${response2.body[0]._id}/upvote`)
+      .send(upvoteRequest);
+    expect(response3.status).toBe(200);
+
+    // Check if post was upvoted
+    const response4 = await supertest(app).get('/posts');
+    expect(response4.body[0].upvotes_clap).toBe(-1);
+    done();
+  });
 });
