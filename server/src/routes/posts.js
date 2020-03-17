2525fe98db3f9a8d5897d8e75da82843b2644c30
GrunWong <grunwong@gmail.com>
2020-03-17 14:36:29 +1300

    Changed res.status to only send code upon success

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -46,7 +46,7 @@ router.delete('/:id', async (req, res) => {
     await Post.findOneAndDelete({
       _id: req.params.id,
     });
-    res.status(200).json({ message: 'ok' });
+    res.status(200).send();
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
