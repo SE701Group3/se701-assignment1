e87cb99bf5b25b85a94486007f4ced11dcd63965
WaiHaiDereMAC <tyger.kong@hotmail.com>
2020-03-17 16:47:45 +1300

    Vote counter updates on frontend

--- frontend/src/pages/Frontpage/Post.jsx
+++ frontend/src/pages/Frontpage/Post.jsx
@@ -14,6 +14,9 @@ const Post = ({ id, title, content, upvotes, downvotes, claps, handleVote }) => 
   const [upvoteClap, setUpvoteClap] = useState(false);
   const [upvoteLaugh, setUpvoteLaugh] = useState(false);
   const [upvoteSad, setUpvoteSad] = useState(false);
+  const [clapCount, setClapCount] = useState(claps);
+  const [smileCount, setSmileCount] = useState(upvotes);
+  const [sadCount, setSadCount] = useState(downvotes);
 
   return (
     <Card className={styles.root}>
@@ -24,32 +27,59 @@ const Post = ({ id, title, content, upvotes, downvotes, claps, handleVote }) => 
               className={styles['post-button']}
               onClick={() => {
                 handleVote({ id, upvote_type: 'clap', upvote: !upvoteClap });
+                if (upvoteClap) {
+                  setClapCount(clapCount - 1);
+                } else {
+                  setClapCount(clapCount + 1);
+                }
                 setUpvoteClap(!upvoteClap);
               }}
             >
               <img src={ClapImg} alt="clap-img" className={styles['post-icon']} />
             </Button>
-            <div className={styles.counter}>{claps}</div>
+            {upvoteClap ? (
+              <div className={`${styles.counter} ${styles.clicked}`}>{clapCount}</div>
+            ) : (
+              <div className={styles.counter}>{clapCount}</div>
+            )}
             <Button
               className={styles['post-button']}
               onClick={() => {
                 handleVote({ id, upvote_type: 'laugh', upvote: !upvoteLaugh });
+                if (upvoteLaugh) {
+                  setSmileCount(smileCount - 1);
+                } else {
+                  setSmileCount(smileCount + 1);
+                }
                 setUpvoteLaugh(!upvoteLaugh);
               }}
             >
               <img src={SmileImg} alt="smile-img" className={styles['post-icon']} />{' '}
             </Button>
-            <div className={styles.counter}>{upvotes}</div>
+            {upvoteLaugh ? (
+              <div className={`${styles.counter} ${styles.clicked}`}>{smileCount}</div>
+            ) : (
+              <div className={styles.counter}>{smileCount}</div>
+            )}
             <Button
               className={styles['post-button']}
               onClick={() => {
                 handleVote({ id, upvote_type: 'sad', upvote: !upvoteSad });
+                if (upvoteSad) {
+                  setSadCount(sadCount - 1);
+                } else {
+                  setSadCount(sadCount + 1);
+                }
                 setUpvoteSad(!upvoteSad);
               }}
             >
               <img src={SadImg} alt="sad-img" className={styles['post-icon']} />{' '}
             </Button>
-            <div className={styles.counter}>{downvotes}</div>
+            {upvoteSad ? (
+              <div className={`${styles.counter} ${styles.clicked}`}>{sadCount}</div>
+            ) : (
+              <div className={styles.counter}>{sadCount}</div>
+            )}
           </div>
         </div>
         <CardContent>
