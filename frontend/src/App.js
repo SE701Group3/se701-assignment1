0e7c63997a87ee9b3b9085ad68ebd1437cdfec7f
wshin387 <william-shin-387@gmail.com>
2020-04-02 19:35:46 +1300

    add Login page containing login with google button, returns info about user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import firebase from 'firebase/app';
 
 import './App.css';
 import routes from './routes/routes';
+
+import firebaseConfig from './firebaseConfig';
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
