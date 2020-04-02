dd326bd1a0471031f383f4f420689c987290ef48
wshin387 <william-shin-387@gmail.com>
2020-04-02 19:35:46 +1300

    add Login page containing login with google button, returns info about user

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -6,7 +6,13 @@ const uiConfig = {
   signInFlow: 'popup',
   signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   callbacks: {
+<<<<<<< 777c1ec7d7cab2c58e562879ab3cd397808329a5
     signInSuccessWithAuthResult: () => {
+=======
+    signInSuccessWithAuthResult: (authResult, redirectUrl) => {
+      console.log(authResult);
+      console.log(redirectUrl);
+>>>>>>> add Login page containing login with google button, returns info about user
       return false;
     },
   },
