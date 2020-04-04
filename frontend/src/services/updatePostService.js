4e4ead7a9ef5161c78d9d26bd1b26dcedd61b9e7
DavidChen <David.Chen@water.co.nz>
2020-04-05 11:20:38 +1200

    added a service which allows updating post details

---
+++ frontend/src/services/updatePostService.js
@@ -0,0 +1,31 @@
+import { getPostRoute } from './apiRoutes';
+
+/* eslint-disable no-underscore-dangle */
+export class UpdatePostError extends Error {}
+
+export default async (id, title, body) => {
+  const parameters = {
+    id,
+    title,
+    body,
+  };
+
+  const requestBody = JSON.stringify(parameters);
+
+  const response = await fetch(getPostRoute(id), {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    method: 'PUT',
+    body: requestBody,
+  });
+
+  if (!response.ok) {
+    const { message } = await response.json();
+    if (message.includes('is required')) {
+      throw new UpdatePostError(`Please ensure all fields are filled in`);
+    }
+    throw new UpdatePostError(message);
+  }
+};
