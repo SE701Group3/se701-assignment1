bdaf2fe2b0b6a3bcd1371711acafb157557483f4
wshin387 <william-shin-387@gmail.com>
2020-04-12 15:56:35 +1200

    run lintfix

--- server/src/middleware/firebaseAuth.js
+++ server/src/middleware/firebaseAuth.js
@@ -1,22 +1,22 @@
 const admin = require('firebase-admin');
 
 function firebaseAuthMiddleware(req, res, next) {
-    const authorization = req.header('Authorization');
-    if (authorization) {
-        let token = authorization.split(' ');
-        admin.auth().verifyIdToken(token[1])
-            .then((decodedToken) => {
-                req.user = decodedToken;
-                next();
-            })
-            .catch(err => {
-                log(err);
-                res.sendStatus(401);
-            });
-    } else {
-        log('Authorization header is not found');
-        res.sendStatus(401);
-    }
+  const authorization = req.header('Authorization');
+  if (authorization) {
+    const token = authorization.split(' ');
+    admin
+      .auth()
+      .verifyIdToken(token[1])
+      .then(decodedToken => {
+        req.user = decodedToken;
+        next();
+      })
+      .catch(err => {
+        res.status(401).send(err);
+      });
+  } else {
+    res.send(401).send('Authorization header is not found');
+  }
 }
 
 module.exports = firebaseAuthMiddleware;
