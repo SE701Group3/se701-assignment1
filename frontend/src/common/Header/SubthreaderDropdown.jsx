2d11e3352d0ec55ea59e10956af77bda6ce84ba6
nidhineshnand <nnidhinesh@gmail.com>
2020-04-15 11:38:12 +1200

    Added button for adding new subthreads and ensured that subthread module only shows up on home page

--- frontend/src/common/Header/SubthreaderDropdown.jsx
+++ frontend/src/common/Header/SubthreaderDropdown.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Select, InputLabel } from '@material-ui/core';
 import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import AddOutlinedIcon from '@material-ui/icons/AddOutlined';
 
 import './SubthreaderDropdown.css';
 
@@ -12,10 +14,14 @@ const SubthreaderDropdown = ({ retrievedSubthreaders, changeSubthread }) => {
     changeSubthread(event.target.value);
   };
 
-  if (window.location.pathname === '/post') return null;
+  if (!(window.location.pathname === '/')) return null;
 
   return (
     <div className="dropbox-parent">
+      <IconButton>
+        <AddOutlinedIcon className="icon" />
+      </IconButton>
+
       <InputLabel className="child-elements" id="label">
         Subthreaders
       </InputLabel>
