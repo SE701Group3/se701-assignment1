793d94d98bdeb26fcd2981ae73b4b00063acedc1
nidhineshnand <nnidhinesh@gmail.com>
2020-04-14 12:41:40 +1200

    Changed value for all to empty

--- frontend/src/common/Header/SubthreaderDropdown.jsx
+++ frontend/src/common/Header/SubthreaderDropdown.jsx
@@ -20,7 +20,7 @@ const SubthreaderDropdown = ({ retrievedSubthreaders, changeSubthread }) => {
         Subthreaders
       </InputLabel>
       <Select className="child-elements" labelId="label" value={thread} onChange={handleChange}>
-        <MenuItem value="All"> All </MenuItem>
+        <MenuItem value=""> All </MenuItem>
         {retrievedSubthreaders.map(sub => (
           <MenuItem value={sub.title}> {sub.title} </MenuItem>
         ))}
