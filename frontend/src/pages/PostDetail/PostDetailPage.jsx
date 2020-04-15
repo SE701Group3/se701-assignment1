91cce4da3eecf1e6f7f616e4de1946cc7332b775
Kungeng Wu <43196558+Kwu849@users.noreply.github.com>
2020-04-15 16:41:19 +1200

    Updated subcomments UI (#170)
    * added collapsable comments
    
    * made subcomment container larger
    
    * merged with master

--- frontend/src/pages/PostDetail/PostDetailPage.jsx
+++ frontend/src/pages/PostDetail/PostDetailPage.jsx
@@ -4,7 +4,10 @@ import { Container } from '@material-ui/core';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import { usePromiseTracker } from 'react-promise-tracker';
-
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import PostDetail from './PostDetail';
 import Comment from './Comment';
 import CreateCommentModal from './CreateCommentModal';
@@ -24,17 +27,44 @@ const formatDate = date => {
 const nestComments = (commentList, setModal, setParentID) => {
   if (commentList && Array.isArray(commentList) && commentList.length) {
     return commentList.map(comment => (
-      <Container key={`${comment.id}-container-key`}>
-        <Comment
-          body={comment.body}
-          dateCreated={formatDate(comment.date_created)}
-          setModal={() => {
-            setModal(true);
-            setParentID(comment._id);
-          }}
-          key={`${comment.id}-key`}
-        />
-        {nestComments(comment.children, setModal, setParentID)}
+      <Container className={styles.commentContainer} key={`${comment.id}-container-key`}>
+        {comment.children.length ? (
+          <ExpansionPanel defaultExpanded>
+            <ExpansionPanelSummary
+              className={styles.comment}
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Comment
+                body={comment.body}
+                dateCreated={formatDate(comment.date_created)}
+                setModal={() => {
+                  setModal(true);
+                  setParentID(comment._id);
+                }}
+                key={`${Math.floor(Math.random() * 100)}`}
+              />
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails className={styles.subcommentContainer}>
+              {nestComments(comment.children, setModal, setParentID)}
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        ) : (
+          <ExpansionPanel expanded={false}>
+            <ExpansionPanelSummary>
+              <Comment
+                body={comment.body}
+                dateCreated={formatDate(comment.date_created)}
+                setModal={() => {
+                  setModal(true);
+                  setParentID(comment._id);
+                }}
+                key={`${Math.floor(Math.random() * 100)}`}
+              />
+            </ExpansionPanelSummary>
+          </ExpansionPanel>
+        )}
       </Container>
     ));
   }
@@ -57,30 +87,58 @@ const PostDetailPage = ({
   return (
     <div>
       <Header handleSearch={handleSearch} />
-
       <PostDetail
         postToDisplay={postToDisplay}
         handleVote={handleVote}
         getPostInformationOnLoad={getPostInformationOnLoad}
       />
-      {promiseInProgress ? (
-        <LoadingIndicator />
-      ) : (
-        commentsToDisplay.map(comment => (
-          <Container maxWidth="sm" key={`${Math.floor(Math.random() * 100)}-container-key`}>
-            <Comment
-              body={comment.body}
-              dateCreated={formatDate(comment.date_created)}
-              setModal={() => {
-                setModal(true);
-                setParentID(comment._id);
-              }}
-              key={`${Math.floor(Math.random() * 100)}`}
-            />
-            {nestComments(comment.children, setModal, setParentID)}
-          </Container>
-        ))
-      )}
+      <div className={styles.comments}>
+        {promiseInProgress ? (
+          <LoadingIndicator />
+        ) : (
+          commentsToDisplay.map(comment => (
+            <Container maxWidth="md" key={`${Math.floor(Math.random() * 100)}-container-key`}>
+              {comment.children.length ? (
+                <ExpansionPanel>
+                  <ExpansionPanelSummary
+                    className={styles.comment}
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                  >
+                    <Comment
+                      body={comment.body}
+                      dateCreated={formatDate(comment.date_created)}
+                      setModal={() => {
+                        setModal(true);
+                        setParentID(comment._id);
+                      }}
+                      key={`${Math.floor(Math.random() * 100)}`}
+                    />
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails className={styles.subcommentContainer}>
+                    {nestComments(comment.children, setModal, setParentID)}
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              ) : (
+                <ExpansionPanel expanded={false}>
+                  <ExpansionPanelSummary>
+                    <Comment
+                      body={comment.body}
+                      dateCreated={formatDate(comment.date_created)}
+                      setModal={() => {
+                        setModal(true);
+                        setParentID(comment._id);
+                      }}
+                      key={`${Math.floor(Math.random() * 100)}`}
+                    />
+                  </ExpansionPanelSummary>
+                </ExpansionPanel>
+              )}
+            </Container>
+          ))
+        )}
+      </div>
       <Fab
         classes={{
           root: styles.addButton,
