9b398e442e9175bbee9bf7ad9089f0e768e5e420
wshin387 <william-shin-387@gmail.com>
2020-04-17 14:57:40 +1200

    json plaintext secret

--- server/src/firebase.js
+++ server/src/firebase.js
@@ -1,6 +1,8 @@
 const admin = require('firebase-admin');
 
-const serviceAccountJson = JSON.parse(Buffer.from(process.env.SERVICE_ACCOUNT, 'base64'));
+const serviceAccountJson = JSON.parse(process.env.SERVICE_ACCOUNT);
+// eslint-disable-next-line no-console
+// const serviceAccountJson = JSON.parse(Buffer.from(process.env.SERVICE_ACCOUNT, 'base64'));
 
 const firebaseApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccountJson),
