becbec6fb0f55120d38021a517c323e7f1bf557a
wshin387 <william-shin-387@gmail.com>
2020-04-17 13:50:31 +1200

    print test env variable

--- server/src/firebase.js
+++ server/src/firebase.js
@@ -1,6 +1,8 @@
 const admin = require('firebase-admin');
 
 // eslint-disable-next-line no-console
+console.info(process.env.testvar);
+// eslint-disable-next-line no-console
 console.info(process.env.SERVICE_ACCOUNT);
 const serviceAccountJson = JSON.parse(Buffer.from(process.env.SERVICE_ACCOUNT, 'base64'));
 
