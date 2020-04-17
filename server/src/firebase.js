10a79a343cd1e685302754147532476a8f3e5962
wshin387 <william-shin-387@gmail.com>
2020-04-17 13:40:14 +1200

    revert

--- server/src/firebase.js
+++ server/src/firebase.js
@@ -1,7 +1,5 @@
 const admin = require('firebase-admin');
 
-// eslint-disable-next-line no-console
-console.log(process.env.SERVICE_ACCOUNT);
 const serviceAccountJson = JSON.parse(Buffer.from(process.env.SERVICE_ACCOUNT, 'base64'));
 
 const firebaseApp = admin.initializeApp({
