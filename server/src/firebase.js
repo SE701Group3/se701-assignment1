2f55dd21c468220a3fac4f631a546404cd0174ad
wshin387 <william-shin-387@gmail.com>
2020-04-17 14:14:02 +1200

    remove prints

--- server/src/firebase.js
+++ server/src/firebase.js
@@ -1,11 +1,5 @@
 const admin = require('firebase-admin');
 
-// eslint-disable-next-line no-console
-console.info(process.env.testvar2);
-// eslint-disable-next-line no-console
-console.info(process.env.testvar);
-// eslint-disable-next-line no-console
-console.info(process.env.SERVICE_ACCOUNT);
 const serviceAccountJson = JSON.parse(Buffer.from(process.env.SERVICE_ACCOUNT, 'base64'));
 
 const firebaseApp = admin.initializeApp({
